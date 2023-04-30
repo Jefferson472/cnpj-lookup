df3d8b70d2720082666c917e8e235e2d0ada4ca0
Jefferson Miranda <jefferson472@hotmail.com>
2023-04-30 12:26:04 -0300

    fix: add qualificacao field

--- src/apps/cnpj_lookup/models/Socio.py
+++ src/apps/cnpj_lookup/models/Socio.py
@@ -6,6 +6,7 @@ from apps.cnpj_lookup.models import Empresa
 class Socio(models.Model):
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name='socios')
     nome = models.CharField(max_length=200)
+    qualificacao = models.CharField(max_length=200)
 
     def __str__(self):
         return f'{self.nome} ({self.empresa.razao_social})'
