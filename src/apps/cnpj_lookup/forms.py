d249e101eab0b646197d43b25389f9fcbf6b764f
Jefferson Miranda <jefferson472@hotmail.com>
2023-05-04 12:46:27 -0300

    feat: melhorando layout da pagina de detalhes

--- src/apps/cnpj_lookup/forms.py
+++ src/apps/cnpj_lookup/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Column
+
 from .models import Empresa, Socio
 
 
@@ -9,14 +12,55 @@ class EmpresaForm(forms.ModelForm):
 
 
 class EmpresaDetalhesForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.layout = Layout(
+            Row(
+                Column('nome_fantasia', css_class='form-group col-md-6 mb-0'),
+                Column('razao_social', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+            Row(
+                Column('cnpj', css_class='form-group col-md-6 mb-0'),
+                Column('inscricao_estadual', css_class='form-group col-md-6 mb-0'),
+            ),
+            Row(
+                Column('telefone', css_class='form-group col-md-6 mb-0'),
+                Column('email', css_class='form-group col-md-6 mb-0'),
+            ),
+            Row(
+                Column('cnae_principal', css_class='form-group col-md-6 mb-0'),
+            ),
+            Row(
+                Column('endereco', css_class='form-group mb-0'),
+            ),
+            Row(
+                Column('bairro', css_class='form-group mb-0'),
+                Column('cidade', css_class='form-group mb-0'),
+                Column('estado', css_class='form-group mb-0'),
+                Column('cep', css_class='form-group mb-0'),
+            ),
+        )
+
     class Meta:
         model = Empresa
         fields = ['razao_social', 'nome_fantasia', 'endereco', 'bairro',
                   'cidade', 'estado', 'cep', 'telefone', 'email',
-                  'cnae_principal', 'inscricao_estadual']
+                  'cnae_principal', 'inscricao_estadual', 'cnpj']
 
 
 class SocioForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.layout = Layout(
+            Row(
+                Column('nome', css_class='form-group mb-0'),
+                Column('qualificacao', css_class='form-group mb-0'),
+            )
+        )
+
     class Meta:
         model = Socio
         fields = ['nome', 'qualificacao']
